4320893361259b8b82db93bc88e893e61de31cb4
myusername <mahfuzur35-2207@diu.edu.bd>
2021-03-24 21:44:01 +0600

    templates directory added

--- dj4e_mysite/polls/views.py
+++ dj4e_mysite/polls/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
 
 # Create your views here.
+
+class Index(View):
+    
 
 def index(request):
     return HttpResponse("Mahfuzur Rahman")
