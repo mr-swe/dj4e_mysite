4d807ab1db21ad3474ce137c8cb1ab42bce86d9e
myusername <mahfuzur35-2207@diu.edu.bd>
2021-03-24 22:51:11 +0600

    index page created

--- dj4e_mysite/polls/views.py
+++ dj4e_mysite/polls/views.py
@@ -2,9 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import ListView
 from django.views import View
+from .models import Question, Choice
 
 
 # Create your views here.
 
 class Index(ListView):
     template_name = "polls/index.html"
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
