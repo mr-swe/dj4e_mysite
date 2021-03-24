f3d2f8f412ab350578891967d2bef7d1db4627f9
myusername <mahfuzur35-2207@diu.edu.bd>
2021-03-24 21:41:47 +0600

    polls apps running successfully

--- dj4e_mysite/dj4e_mysite/urls.py
+++ dj4e_mysite/dj4e_mysite/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    # path: polls/ urls.py
+    path('', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
