db8fb1b343a753652fd40d5ba469483b66d3224c
myusername <mahfuzur35-2207@diu.edu.bd>
2021-03-25 09:20:28 +0600

    vote done

--- dj4e_mysite/polls/urls.py
+++ dj4e_mysite/polls/urls.py
@@ -4,8 +4,12 @@ from . import views
 
 app_name = "polls"
 urlpatterns = [
-    # path: polls/ views/ Index
+    # path: polls/ views/ Index(class)
     path('', views.Index.as_view(), name='index'),
-    # path: polls/ views/ Detail
+    # path: polls/ views/ Detail(class)
     path('detail/<int:pk>/', views.Detail.as_view(), name='detail'),
+    # path: polls/ views/ Result(class)
+    path('<int:pk>result/', views.Result.as_view(), name='result'),
+    # path: polls/ views/ vote(function)
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- dj4e_mysite/polls/views.py
+++ dj4e_mysite/polls/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.views.generic import ListView, DetailView
 from django.views import View
 from .models import Question, Choice
@@ -19,3 +20,27 @@ class Index(ListView):
 class Detail(DetailView):
     template_name = 'polls/detail.html'
     model = Question
+
+
+class Result(DetailView):
+    model = Question
+    template_name = 'polls/result.html'
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:result', args=(question.id,)))
