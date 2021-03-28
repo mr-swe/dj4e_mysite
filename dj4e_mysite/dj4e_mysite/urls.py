6281ba03b37dd02a901d902d69d8a88429d201e6
myusername <mahfuzur35-2207@diu.edu.bd>
2021-03-28 23:04:47 +0600

    autos urls linked

--- dj4e_mysite/dj4e_mysite/urls.py
+++ dj4e_mysite/dj4e_mysite/urls.py
@@ -5,6 +5,8 @@ from django.views.generic import TemplateView
 urlpatterns = [
     # path: home
     path('', TemplateView.as_view(template_name='home/main.html')),
+    # path: autos? urls.py
+    path('autos/', include('autos.urls')),
     # path: hello/ urls.py
     path('hello/', include('hello.urls')),
     # path: polls/ urls.py
