54e24da98cef6af5fc2ee3ffdecd5041a9f90233
myusername <mahfuzur35-2207@diu.edu.bd>
2021-03-31 14:26:29 +0600

    cat list page updated

--- dj4e_mysite/cats/urls.py
+++ dj4e_mysite/cats/urls.py
@@ -5,5 +5,5 @@ from . import views
 
 app_name = "cats"
 urlpatterns = [
-
+    path('', views.CatView.as_view(), name='all_cat')
 ]
--- dj4e_mysite/cats/views.py
+++ dj4e_mysite/cats/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views import View
+
+from .models import Breed, Cat
+
 
 # Create your views here.
+class CatView(View):
+    def get(self, request):
+        bc = Breed.objects.all().count()
+        cl = Cat.objects.all()
+        ctx = {
+            'breed_count': bc,
+            'cat_list': cl,
+        }
+        return render(request, 'cats/cat_list.html', ctx)
