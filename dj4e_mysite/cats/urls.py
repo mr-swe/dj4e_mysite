e91243679b56b292c67beb2da3c9ee0597f65953
myusername <mahfuzur35-2207@diu.edu.bd>
2021-03-31 15:11:16 +0600

    Cat Created, update, delete successfully done

--- dj4e_mysite/cats/urls.py
+++ dj4e_mysite/cats/urls.py
@@ -5,5 +5,8 @@ from . import views
 
 app_name = "cats"
 urlpatterns = [
-    path('', views.CatView.as_view(), name='all_cat')
+    path('', views.CatView.as_view(), name='all_cat'),
+    path('cats/create/', views.CatCreate.as_view(), name='cat_create'),
+    path('cats/<int:pk>/update/', views.CatUpdate.as_view(), name='cat_update'),
+    path('cats/<int:pk>/delete/', views.CatDelete.as_view(), name='cat_delete'),
 ]
--- dj4e_mysite/cats/views.py
+++ dj4e_mysite/cats/views.py
@@ -1,11 +1,14 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import View
+from django.views.generic import CreateView, UpdateView, DeleteView
 
 from .models import Breed, Cat
 
 
 # Create your views here.
-class CatView(View):
+class CatView(LoginRequiredMixin, View):
     def get(self, request):
         bc = Breed.objects.all().count()
         cl = Cat.objects.all()
@@ -14,3 +17,24 @@ class CatView(View):
             'cat_list': cl,
         }
         return render(request, 'cats/cat_list.html', ctx)
+
+
+class CatCreate(LoginRequiredMixin, CreateView):
+    model = Cat
+    template_name = 'cats/cat_form.html'
+    fields = '__all__'
+    success_url = reverse_lazy('cats:all_cat')
+
+
+class CatUpdate(LoginRequiredMixin, UpdateView):
+    model = Cat
+    template_name = 'cats/cat_form.html'
+    fields = '__all__'
+    success_url = reverse_lazy('cats:all_cat')
+
+
+class CatDelete(LoginRequiredMixin, DeleteView):
+    model = Cat
+    template_name = 'cats/cat_confirm_delete.html'
+    fields = '__all__'
+    success_url = reverse_lazy('cats:all_cat')
