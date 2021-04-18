7111ede5d3c4fc5236aabba6f33e9ff8d7092221
Mahfuzur Rahman <mahfuzur35-2207@diu.edu.bd>
2021-04-18 12:36:20 +0600

    views created in ads app

--- dj4e_mysite/ads/views.py
+++ dj4e_mysite/ads/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from .owner import OwnerListView, OwnerDetailView, OwnerCreateView, OwnerUpdateView, OwnerDeleteView
+from .models import Ad
+
 
 # Create your views here.
+class AdListView(OwnerListView):
+    model = Ad
+    template_name = 'ads/article_list.html'
+
+
+class AdDetailView(OwnerDetailView):
+    model = Ad
+    template_name = 'ads/article_detail.html'
+
+
+class AdCreateView(OwnerCreateView):
+    model = Ad
+    fields = ['title', 'text']
+    template_name = 'ads/article_form.html'
+
+
+class AdUpdateView(OwnerUpdateView):
+    model = Ad
+    fields = ['title', 'text']
+    template_name = 'ads/article_form.html'
+
+
+class AdDeleteView(OwnerDeleteView):
+    model = Ad
+    template_name = 'ads/article_confirm_delete.html'
